e1d62f9a55c79d51b557030c147d82fe69a1db2e
Cocona.M <53136890+Cocona-Matsuda@users.noreply.github.com>
2020-07-25 23:05:20 +0900

    プルダウン

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,7 @@
 
 const log = document.querySelector('html').lang;
 
+//optionタグにselected属性を追加する
 if(lang === 'ja'){
     document.querySelector('option[value="japanese.html"]').selected = true;
 }else if(lang === 'en'){
