1afad3022789e5b2b80a95fb94745fa4c479c47d
Cocona-Matsuda <skyebell13rei@gmail.com>
2020-09-19 13:56:46 +0900

    練習

---
+++ python/defadd.py
@@ -0,0 +1,15 @@
+def add(num_1,num_2):
+    print(num_1+num_2)
+add(10,20)
+
+def show(name):
+    return name + "さんこんにちは"
+
+text = show('太郎')
+print(text)
+
+#4-2
+def ad(num_1,num_2):
+    return num_1+num_2
+
+print(ad(10,20))
