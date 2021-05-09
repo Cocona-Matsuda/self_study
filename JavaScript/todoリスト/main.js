21b592e7bd207a5feab516a8cf8f3dde1fa3281f
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-05-09 18:22:30 +0900

    todo.js

---
+++ JavaScript/todoリスト/main.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const ul = document.getElementById('ul');
+    const addBtn = document.getElementById('add-btn');
+
+    addBtn.addEventListener('click',function(){
+        const todo = new ToDoList(ul);
+    });
+});
+
+class ToDoList {
+    constructor(ul){
+        this.ul = ul;
+        const li = document.createElement('li');
+        const click = document.createElement('div');
+        const text = document.createElement('p');
+        const button = document.createElement('button');
+        this.li = li;
+        this.check = check;
+        this.text = text;
+        this.delBtn = button;
+        this._setAttr(this._getValue());
+        this._addEl(this.ul);
+        this.ListEvents();
+    }
+    _getValue(){
+        this.formText = document.getElementById('form-text');
+        return this.formText.nodeValue;
+    }
+    _setAttr(txt){
+        this.li.classList.add('list');
+        this.check.classList.add('list__check');
+        this.text.classList.add('list__text');
+        this.text.textContent = txt;
+        this.delBtn.classList.add('list_delate');
+        this.delBtn.type = 'button';
+        this.delBtn.textContent = 'delate';
+    }
+    _addEl(ul){
+        this.li.appendChild(this.check);
+        this.li.appendChild(this.text);
+        this.li.appendChild(this.delBtn);
+        ul.appendChild(this.li);
+        this.formText.value = '';
+    }
+    _ListEvents() {
+        this.delBtn.addEventListener('click', function() {
+            this.parentElement.remove();
+        });
+        this.check.addEventListener('click', function() {
+            this.parentElement.classList.add('done');
+        });
+   }
+}
